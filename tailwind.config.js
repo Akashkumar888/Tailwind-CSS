7caf840151ab775c78ad7eec0b5dcc43618aaf84
Akashkumar888 <akashgkr12@gmail.com>
2025-05-24 18:16:20 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#ee0181',
+        'anuj':'#1e4235'
+      },
+      fontFamily:{
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
